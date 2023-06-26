e6b3b47758721b7a70a65dbce270a7cb913c97bb
safym <osafym@gmail.com>
2023-06-26 08:32:25 +0300

    add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
+import { MeetupPageComponent } from "./pages/meetup-page/meetup-page.component";
+
+import { AuthGuard } from "./guards/auth.guard";
+import { UsersPageComponent } from "./pages/users-page/users-page.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "login", component: LoginPageComponent },
+  { path: "meetups", component: MeetupPageComponent, canActivate: [AuthGuard] },
+  { path: "users", component: UsersPageComponent, canActivate: [AuthGuard] },
+  { path: "404", component: NotFoundPageComponent },
+  { path: "**", redirectTo: "/404" },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MeetupPageComponent } from './pages/meetup-page/meetup-page.component';
+import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeetupPageComponent,
+    UsersPageComponent,
+    NotFoundPageComponent,
+    LoginPageComponent,
+    HeaderComponent,
+    LoginFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
