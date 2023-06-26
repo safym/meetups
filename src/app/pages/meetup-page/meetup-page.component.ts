bb79c2ce1269ac974469c33d57d77f47da8f90a3
safym <osafym@gmail.com>
2023-06-26 08:29:13 +0300

    add meetup page template

---
+++ src/app/pages/meetup-page/meetup-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meetup-page',
+  templateUrl: './meetup-page.component.html',
+  styleUrls: ['./meetup-page.component.scss']
+})
+export class MeetupPageComponent {
+
+}
