f836ff404e995c890a119adf4151ac9c9d718337
safym <osafym@gmail.com>
2023-06-27 10:33:54 +0300

    add header temp element for future improvements

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from "src/app/services/auth.service";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   constructor(private authService: AuthService) {}
