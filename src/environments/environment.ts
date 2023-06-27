a8dadda4b4210eda8d58186f92df7b4a7e30fe13
Mayya <99616798+safym@users.noreply.github.com>
2023-06-27 18:06:47 +0300

    Revert "Feature header"

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-import { EnvironmentInterface } from "./environment-interface";
-
-export const environment: EnvironmentInterface = {
-  production: false,
-  baseUrl: "https://api.fit-meetups.ru",
-};
