f72108467c7290261cf7b885ecb67663cf825ba8
safym <osafym@gmail.com>
2023-06-27 23:12:46 +0300

    fix header styles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-import { AuthService } from "src/app/services/auth.service";
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   constructor(public authService: AuthService) {}
 }
+
