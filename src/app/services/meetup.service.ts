432c8f2e62069f2fc618304fd98fe79bea4aa56a
safym <osafym@gmail.com>
2023-06-27 10:29:02 +0300

    fix meetup model

--- src/app/services/meetup.service.ts
+++ src/app/services/meetup.service.ts
@@ -1,37 +1,64 @@
 import { Injectable } from '@angular/core';
-import { IMeetup } from '../models/meetup.interface';
+import { IMeetup } from '../models/meetup/meetup.interface';
 import { Meta } from '@angular/platform-browser';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Meetup } from '../models/meetup/meetup';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MeetupService {
-  private meetupList: IMeetup[] = [
+  private _meetupList: Meetup[] = [
     {
       id: 1,
-      name: 'name',
-      date: new Date,
-      location: 'location',
-      shortDescription: 'shortDescription',
-      detailedDescription: 'detailedDescription',
-      targetAudience: 'targetAudience',
-      requirements: 'requirements',
-      agenda: 'agenda',
-      reasonToAttend: 'reasonToAttend',
-      organizer: {
-        fisrtName: 'john',
-        lastName: 'doe',
-      }
+      name: 'RxJS',
+      description: 'Расскажем об основах RxJS',
+      location: 'Переговорка 4',
+      target_audience: 'Разработчики, аналитики',
+      need_to_know: 'Ядренную физику',
+      will_happen: 'Будем готовить пиццу',
+      reason_to_come: 'Надо',
+      time: '2023-06-13T19:14:13.094Z',
+      duration: 90,
+      createdBy: 1,
+      owner: {
+        id: 1,
+        email: 'pam@dundermifflin.com',
+        password: 'password',
+        fio: 'password',
+      },
+      users: [
+        {
+          id: 1,
+          email: 'pam@dundermifflin.com',
+          password: 'password',
+          fio: 'password',
+        },
+      ],
     },
-  ]
+  ];
 
-  getMeetupList(): IMeetup[] {
-    return this.meetupList;
+  constructor(private http: HttpClient) {}
+
+  get meetupList(): Meetup[] {
+    return this._meetupList;
   }
 
-  addMeetup(meetup: IMeetup) {
-    this.meetupList.push(meetup)
+  set todoList(meetupList: Meetup[]) {
+    this._meetupList = meetupList;
   }
 
-  constructor() { }
+  loadTodoList(): Observable<any> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.get<any>(`${environment.baseUrl}/meetup`, {
+      headers,
+    });
+  }
+
+  addMeetup(meetup: Meetup) {
+    this._meetupList.push(meetup);
+  }
 }
