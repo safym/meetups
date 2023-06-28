9d9729295da7083b6fe13ccf6dc076c2ed9b0943
safym <osafym@gmail.com>
2023-06-28 10:04:34 +0300

    fix login form styles

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from "src/app/services/auth.service";
 @Component({
   selector: "app-login-form",
   templateUrl: "./login-form.component.html",
+  styleUrls: ['./login-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginFormComponent {
