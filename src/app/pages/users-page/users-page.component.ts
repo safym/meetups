e3200f233904ee3affafffd1d1647babf70ded27
safym <osafym@gmail.com>
2023-06-28 10:03:31 +0300

    add page style class to pages

--- src/app/pages/users-page/users-page.component.ts
+++ src/app/pages/users-page/users-page.component.ts
@@ -4,5 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.scss'],
+  host: {
+    class: 'page',
+  },
 })
 export class UsersPageComponent {}
