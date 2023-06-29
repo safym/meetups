b0ba815aeeb98b8a57e857e8e3bbc95ee09f5240
safym <osafym@gmail.com>
2023-06-30 01:12:56 +0300

    upd routing for meetup create/edit page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
 import { MeetupPageComponent } from "./pages/meetup-page/meetup-page.component";
+import { UsersPageComponent } from "./pages/users-page/users-page.component";
+import { MeetupFormPageComponent } from "./pages/meetup-form-page/meetup-form-page.component";
 
 import { AuthGuard } from "./guards/auth.guard";
-import { UsersPageComponent } from "./pages/users-page/users-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "login", component: LoginPageComponent },
   { path: "meetups", component: MeetupPageComponent, canActivate: [AuthGuard] },
   { path: "my-meetups", component: MeetupPageComponent, canActivate: [AuthGuard], data: { isMyMeetups: true } },
+  { path: "meetups/create", component: MeetupFormPageComponent, canActivate: [AuthGuard] },
+  { path: "meetups/edit/:id", component: MeetupFormPageComponent, canActivate: [AuthGuard] },
   { path: "users", component: UsersPageComponent, canActivate: [AuthGuard] },
   { path: "404", component: NotFoundPageComponent },
   { path: "**", redirectTo: "/404" },
