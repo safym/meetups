9c5fae34be8ed5298e329e2d4dd53765d035675a
safym <osafym@gmail.com>
2023-07-01 01:49:56 +0300

    fix meetup form page

--- src/app/pages/meetup-form-page/meetup-form-page.component.ts
+++ src/app/pages/meetup-form-page/meetup-form-page.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-meetup-form-page',
   templateUrl: './meetup-form-page.component.html',
   styleUrls: ['./meetup-form-page.component.scss'],
 })
-export class MeetupFormPageComponent {}
+export class MeetupFormPageComponent implements OnInit {
+  meetupId: number | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.meetupId = Number(this.route.snapshot.params['id']) || null;
+  }
+}
