c66526e2fc45ddf021307a23ef44a77748c5efe9
safym <osafym@gmail.com>
2023-07-01 01:52:20 +0300

    add edit method to meetup form

--- src/app/components/meetup-form/meetup-form.component.ts
+++ src/app/components/meetup-form/meetup-form.component.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
 
 import { WithFormControl } from 'src/app/utils/withFormControl.type';
 
-import { MeetupForm, MeetupFormNullable } from 'src/app/models/meetup/meetup.interface';
+import { Meetup, MeetupFormNullable } from 'src/app/models/meetup/meetup.interface';
 import { MeetupService } from 'src/app/services/meetup.service';
 
 type MeetupFormControls = WithFormControl<MeetupFormNullable>;
@@ -15,6 +15,8 @@ type MeetupFormControls = WithFormControl<MeetupFormNullable>;
   styleUrls: ['./meetup-form.component.scss'],
 })
 export class MeetupFormComponent implements OnInit {
+  @Input() meetupId: number | null = null;
+  isEdit: boolean;
   meetupForm: FormGroup<MeetupFormControls>;
   isLoading = false;
 
@@ -25,36 +27,64 @@ export class MeetupFormComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.isEdit = !!this.meetupId;
     this.initForm();
   }
 
   initForm(): void {
     this.meetupForm = this.fb.group({
-      name: [{ value: 'name new meetup', disabled: false }],
-      short_description: ['short description'],
-      long_description: ['long description'],
-      location: ['location 3'],
-      target_audience: ['target_audience'],
-      need_to_know: ['need_to_know'],
-      will_happen: ['will_happen'],
-      reason_to_come: ['reason_to_come'],
-      // time: moment('2023-01-12T23:18:50.700Z').utc().format('YYYY-MM-DDTHH:mm'),
+      name: [{ value: '', disabled: false }],
+      description: [''],
+      location: [''],
+      target_audience: [''],
+      need_to_know: [''],
+      will_happen: [''],
+      reason_to_come: [''],
       time: [moment().format('YYYY-MM-DDTHH:mm')],
       duration: [60],
     });
+
+    if (this.isEdit) this.patchFormData();
   }
 
   onSubmit(): void {
-    if (this.meetupForm.valid) {
-      const formValue = this.meetupForm.value;
+    if (!this.meetupForm.valid) return;
 
-      this.createMeetup(formValue as MeetupForm);
+    const formValue = this.meetupForm.value;
 
-      this.meetupService.createMeetup(formValue as MeetupForm);
+    if (this.isEdit && this.meetupId) {
+      console.log('edit', formValue);
+      this.editMeetup(this.meetupId, formValue as Meetup);
+    } else {
+      this.createMeetup(formValue as Meetup);
     }
   }
 
-  createMeetup(meetupFormData: MeetupForm): void {
+  editMeetup(id: number, meetupFormData: Meetup): void {
+    this.isLoading = true;
+    this.disableForm();
+
+    this.meetupService
+      .editMeetup(id, meetupFormData)
+      .subscribe({
+        next: response => {
+          console.log(response);
+        },
+        error: error => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('complete');
+        },
+      })
+      .add(() => {
+        this.isLoading = false;
+        this.enableForm();
+        this.cdr.detectChanges();
+      });
+  }
+
+  createMeetup(meetupFormData: Meetup): void {
     this.isLoading = true;
     this.disableForm();
 
@@ -76,6 +106,47 @@ export class MeetupFormComponent implements OnInit {
         this.enableForm();
         this.cdr.detectChanges();
       });
+  }
+
+  patchFormData() {
+    if (!this.meetupId) return;
+
+    this.isLoading = true;
+    this.disableForm();
+
+    this.meetupService
+      .getMeetupFormDataById(this.meetupId)
+      .subscribe({
+        next: response => {
+          const modifiedData = this.getModifiedMeetupData(response);
+
+          this.meetupForm.patchValue(modifiedData);
+        },
+        error: error => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('complete');
+        },
+      })
+      .add(() => {
+        this.isLoading = false;
+        this.enableForm();
+        this.cdr.detectChanges();
+      });
+  }
+
+  getModifiedMeetupData(meetupData: Meetup) {
+    const formatTime = this.getFormatDate(meetupData.time);
+
+    return {
+      ...meetupData,
+      time: formatTime,
+    };
+  }
+
+  getFormatDate(date: string) {
+    return moment(date).utc().format('YYYY-MM-DDTHH:mm');
   }
 
   disableForm() {
