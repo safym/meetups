c5d07eab06af52e8df661fe0e6cee7927bbcd8b7
safym <osafym@gmail.com>
2023-07-02 02:52:16 +0300

    delete trash

--- src/app/pages/meetup-page/meetup-page.component.ts
+++ src/app/pages/meetup-page/meetup-page.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-meetup-page',
   templateUrl: './meetup-page.component.html',
-  styleUrls: ['./meetup-page.component.scss'],
   host: {
     class: 'page',
   },
