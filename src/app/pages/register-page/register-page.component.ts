124fba8aefe47532f57dafb216e747c46c94e487
safym <osafym@gmail.com>
2023-07-02 02:48:32 +0300

    add register page component

---
+++ src/app/pages/register-page/register-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  host: { class: 'page page__register' },
+})
+export class RegisterPageComponent {}
