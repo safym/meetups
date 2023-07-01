ca5d3598e63f4dc52624e02bd01850df78248391
safym <osafym@gmail.com>
2023-07-01 18:24:13 +0300

    fix user models

--- src/app/models/user/user.interface.ts
+++ src/app/models/user/user.interface.ts
@@ -1,11 +1,20 @@
 import { Role } from '../role/role.interface';
 
-export interface IUser {
-  id: number;
+export interface User {
+  id?: number;
   email: string;
   password: string;
   fio: string;
+}
+
+export interface UserResponse extends User {
   createdAt: string;
   updatedAt: string;
   roles: Role[];
+}
+
+export interface UserAuthToken extends UserResponse {
+  id: number;
+  iat: number;
+  exp: number;
 }
--- src/app/models/user/user.ts
+++ src/app/models/user/user.ts
@@ -1,19 +1,19 @@
-import { Role } from '../role/role.interface';
-import { IUser } from './user.interface';
+// import { Role } from '../role/role.interface';
+// import { IUser } from './user.interface';
 
-export class User implements IUser {
-  id: number;
-  email: string;
-  password: string;
-  fio: string;
-  createdAt: string;
-  updatedAt: string;
-  roles: Role[];
+// export class User implements IUser {
+//   id: number;
+//   email: string;
+//   password: string;
+//   fio: string;
+//   createdAt: string;
+//   updatedAt: string;
+//   roles: Role[];
 
-  constructor({ id, email, password, fio }: IUser) {
-    this.id = id;
-    this.email = email;
-    this.password = password;
-    this.fio = fio;
-  }
-}
+//   constructor({ id, email, password, fio }: IUser) {
+//     this.id = id;
+//     this.email = email;
+//     this.password = password;
+//     this.fio = fio;
+//   }
+// }
