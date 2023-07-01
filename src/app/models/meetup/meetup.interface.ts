7d5396671a2883182cebf8bb29356a0c0fcc39bf
safym <osafym@gmail.com>
2023-07-01 21:31:26 +0300

    fix meetup model

--- src/app/models/meetup/meetup.interface.ts
+++ src/app/models/meetup/meetup.interface.ts
@@ -1,5 +1,5 @@
 import { WithNull } from 'src/app/utils/withNull.type';
-import { User } from '../user/user.interface';
+import { UserResponse } from '../user/user.interface';
 
 export interface Meetup {
   name: string;
@@ -17,8 +17,8 @@ export interface MeetupResponse extends Meetup {
   id: number;
   description: string;
   createdBy: number;
-  owner: User;
-  users: User[];
+  owner: UserResponse;
+  users: UserResponse[];
 }
 
 export type MeetupFormNullable = WithNull<Meetup>;
