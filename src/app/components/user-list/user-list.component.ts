8bfb910a650337b36f392cd809f64adbf6f39a8a
safym <osafym@gmail.com>
2023-07-03 02:26:13 +0300

    add user heartbeat

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, take } from 'rxjs';
 
 import { RoleResponse } from 'src/app/models/role.interface';
 import { UserResponse } from 'src/app/models/user.interface';
@@ -9,53 +10,53 @@ import { UserService } from 'src/app/services/user.service';
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
+  private _userListSubscription: Subscription;
+  private _intervalSubscription: Subscription;
+  private _loadingInterval: Subscription;
   userList: UserResponse[] = [];
   roleList: RoleResponse[] = [];
-  isLoading = false;
+  isLoading: boolean = false;
 
   constructor(private userService: UserService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    this.loadUserList();
-    this.getRoleList();
-  }
-
-  getRoleList() {
     this.isLoading = true;
-    this.userService
-      .getRoleList()
-      .subscribe({
-        next: response => {
-          console.log(response);
-          this.roleList = response;
-        },
-        error: error => {
-          console.log(error);
-        },
-        complete: () => {
-          console.log('complete');
-        },
-      })
-      .add(() => {
+    this.getRoleList();
+
+    this._userListSubscription = this.userService.getUserList().subscribe((userList: UserResponse[]) => {
+      this.processUserList(userList);
+    });
+
+    this._intervalSubscription = this.userService.getIntervalSubscription();
+
+    this._loadingInterval = interval(0)
+      .pipe(take(1))
+      .subscribe(() => {
         this.isLoading = false;
-        this.cdr.detectChanges();
       });
   }
 
-  loadUserList(): void {
-    this.isLoading = true;
+  private processUserList(userList: UserResponse[]): void {
+    console.log('userList', userList);
+    this.userList = userList;
+    this.cdr.detectChanges();
+  }
+
+  getRoleList() {
     this.userService
-      .loadUserList()
+      .getRoleList()
       .subscribe({
-        next: (response: UserResponse[]) => {
-          this.userList = response;
-          console.log(this.userList);
+        next: roleList => {
+          this.roleList = roleList;
+        },
+        error: error => {
+          console.error(error);
         },
       })
       .add(() => {
-        this.isLoading = false;
         this.cdr.detectChanges();
       });
   }
@@ -66,16 +67,28 @@ export class UserListComponent implements OnInit {
     this.userService
       .deleteUser(id)
       .subscribe({
-        next: response => {
-          console.log(response);
-          this.loadUserList();
-        },
         error: error => {
-          console.log(error);
+          console.error(error);
         },
       })
       .add(() => {
         this.cdr.detectChanges();
       });
+  }
+
+  trackById(index: number, item: UserResponse): number {
+    return item.id;
+  }
+
+  ngOnDestroy(): void {
+    this.isLoading = false;
+
+    if (this._userListSubscription) {
+      this._userListSubscription.unsubscribe();
+    }
+
+    if (this._intervalSubscription) {
+      this._intervalSubscription.unsubscribe();
+    }
   }
 }
