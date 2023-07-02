aeb41c1961d6de7db77fb5aeb7036141b238e792
safym <osafym@gmail.com>
2023-07-02 11:34:14 +0300

    fix typo

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, map, of, tap } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
