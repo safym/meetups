e83df26adbe5b5ae9bfb90f18bd0fdf10529dc23
safym <osafym@gmail.com>
2023-07-02 16:49:30 +0300

    fix styles

--- src/app/pages/users-page/users-page.component.ts
+++ src/app/pages/users-page/users-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
-  styleUrls: ['./users-page.component.scss'],
   host: {
     class: 'page',
   },
